c3188bb3335c911547d07042f87bfc02f0271d8f
dprmd <adipr671@gmail.com>
2024-08-08 17:57:52 +0700

    Updates

---
+++ assets/src-pages-SenAiPage-SenAiPage.jsx-CTaKH2pw.js
@@ -0,0 +1,1 @@
+import{j as r}from"./pnpm-react@18.3.1--FUz9cRTN.js";import{H as o}from"./src-pages-SenAiPage-Header-Header.jsx-PdGczBiM.js";import{M as s}from"./src-pages-SenAiPage-Main-Main.jsx-QdlB5yZm.js";import{F as t}from"./src-pages-SenAiPage-Footer-Footer.jsx-EHqsGG2k.js";const n=()=>r.jsxs(r.Fragment,{children:[r.jsx(o,{}),r.jsx(s,{}),r.jsx(t,{})]});export{n as S};

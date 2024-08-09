86a7c0b2887680f5c3b570fd968c0917f5dfe8ad
dprmd <adipr671@gmail.com>
2024-08-09 21:16:09 +0700

    Updates

---
+++ assets/src-pages-SenAiPage-SenAiPage.jsx-B15LiJWJ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./pnpm-react@18.3.1--FUz9cRTN.js";import{H as o}from"./src-pages-SenAiPage-Header-Header.jsx-vU4JM-eC.js";import{M as s}from"./src-pages-SenAiPage-Main-Main.jsx-D_q3taF3.js";import{F as t}from"./src-pages-SenAiPage-Footer-Footer.jsx-CrUza7zH.js";const n=()=>r.jsxs(r.Fragment,{children:[r.jsx(o,{}),r.jsx(s,{}),r.jsx(t,{})]});export{n as S};
---
+++ assets/src-store-defaultState.json-DApXt5Li.js
@@ -0,0 +1,1 @@
+const e="Indonesia",a="id",n="Indonesia",t="yes",g="gemma2-9b-it",o={botLanguage:e,language:a,languageLabel:n,enterIsSend:t,model:g};export{o as d};

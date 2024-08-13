92ed2e4590543ce73df6f56849df1a796f9b55d6
dprmd <adipr671@gmail.com>
2024-08-14 00:18:21 +0700

    Updates

--- assets/src-main.jsx-C24Gf64x.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./pnpm-react@18.3.1--FUz9cRTN.js";import{c as t}from"./pnpm-react-dom@18.3.1_react@18.3.1--DZ4IdBtu.js";import{A as r}from"./src-App.jsx-CC_TD9ri.js";/* empty css                      */import"./pnpm-@axe-core_react@4.9.1--BjGAUd_E.js";import"./src-language-i18n.js-DjSf18bo.js";t.createRoot(document.getElementById("root")).render(o.jsx(r,{}));

f97941eec0bfdb14f8c2df161f6b0b95d97d80a0
dprmd <adipr671@gmail.com>
2024-08-14 16:57:31 +0700

    Updates

--- assets/src-store-defaultState.json-DApXt5Li.js
+++
@@ -1,1 +0,0 @@
-const e="Indonesia",a="id",n="Indonesia",t="yes",g="gemma2-9b-it",o={botLanguage:e,language:a,languageLabel:n,enterIsSend:t,model:g};export{o as d};
---
+++ assets/src-store-defaultState.json-MiJw9RvF.js
@@ -0,0 +1,1 @@
+const e="Indonesia (Informal)",a="id",n="Indonesia",t="yes",o="gemma2-9b-it",g={botLanguage:e,language:a,languageLabel:n,enterIsSend:t,model:o};export{g as d};
